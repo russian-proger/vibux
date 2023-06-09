09b576574f3adeb26dd0da176d81f5308e3cc251
russian-proger <sagitov.2806@mail.ru>
2023-06-09 10:55:33 +0500

    flask production

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ app.config['SECRET_KEY'] = 'lower bound'
 # app.run('localhost', port=8000)
 
 if __name__ == '__main__':
-    socketio.run(app, port=8000)
+    socketio.run(app, port=8000, debug=False)
     # app.run(port=8000)
